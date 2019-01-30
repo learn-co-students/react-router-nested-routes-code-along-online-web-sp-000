4f317cbfd8107ed8735d876243e41f2f3fbb39ec
danmj <daniel.jung@stern.nyu.edu>
2019-01-29 21:02:53 -0500

    dsa

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -3,7 +3,7 @@ import { Route } from 'react-router-dom';
 import MoviesList from '../components/MoviesList';
 import MovieShow from './MovieShow';
 
-const MoviesPage = ({ match, movies }) => (
+export default const MoviesPage = ({ match, movies }) => (
   <div>
     <MoviesList movies={movies} />
     <Route exact path={match.url} render={() => (
@@ -12,5 +12,3 @@ const MoviesPage = ({ match, movies }) => (
   <Route path={`${match.url}/:movieId`} render={routerProps => <MovieShow {...routerProps} movies={movies} /> }/>
   </div>
 )
-
-export default MoviesPage
