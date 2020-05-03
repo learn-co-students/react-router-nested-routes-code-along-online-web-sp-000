4cd8e721350792c6389cb1a7708b488929eae848
cperez1012 <perezchristian1012@gmail.com>
2020-05-02 21:04:25 -0400

    Done.

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const MoviesList = ({ movies }) => {
     const renderMovies = Object.keys(movies).map(movieID =>
-        <Link key={movieID} to={'/movies/${movieID}'}>{movies[movieID].title}</Link>
+        <Link key={movieID} to={`/movies/${movieID}`}>{movies[movieID].title}</Link>
     );
     
     return (
