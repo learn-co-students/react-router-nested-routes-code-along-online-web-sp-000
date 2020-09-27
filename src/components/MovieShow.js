ed59c03d5f9145ca2ac7247e9dd0c2ef2104836f
github username <github email address>
2020-09-27 19:35:02 -0400

    Done.

--- src/components/MovieShow.js
+++ src/components/MovieShow.js
@@ -2,11 +2,12 @@ import React from 'react';
  
 // Here we add `match` to the arguments so we can access the path information 
 // in `routerProps` that is passed from MoviesPage.js 
+ // And here we access the `movieId` stored in match.params to render 
+      // information about the selected movie
 const MovieShow = ({match, movies}) => {
   return (
     <div>
-      // And here we access the `movieId` stored in match.params to render 
-      // information about the selected movie
+     
       <h3>{ movies[match.params.movieId].title }</h3>
     </div>
   );
--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -3,10 +3,11 @@ import { Route } from 'react-router-dom';
 import MoviesList from '../components/MoviesList';
 import MovieShow from '../components/MovieShow';
  
+    // Adding code to show a message to the user to select a movie if they haven't yet
+
 const MoviesPage = ({ match, movies }) => (
   <div>
     <MoviesList movies={movies} />
-    // Adding code to show a message to the user to select a movie if they haven't yet
     <Route exact path={match.url} render={() => <h3>Choose a movie from the list above</h3>}/>
     <Route path={`${match.url}/:movieId`} render={routerProps => <MovieShow {...routerProps} movies={movies} /> }/>
   </div>
