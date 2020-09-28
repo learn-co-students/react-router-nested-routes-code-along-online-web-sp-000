a425c705d876f8fcb3ae7a5b1daaa9065bab25b8
Alvin Coulson <aaacoulson@gmail.com>
2020-09-27 22:59:42 -0400

    Done.

--- src/components/MovieShow.js
+++ src/components/MovieShow.js
@@ -3,7 +3,7 @@ import React from 'react';
 const MovieShow = ({match, movies}) => {
     return (
         <div>
-            <h3>{ movies[match.params.movieId] }</h3>
+            <h3>{ movies[match.params.movieId].title }</h3>
         </div>
     );
 };
