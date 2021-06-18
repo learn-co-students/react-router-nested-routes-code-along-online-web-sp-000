f0fca9d66cfaed094b924a27c76de4b160c73ef6
yarvinh <yarvinhhernandez@gmail.com>
2021-06-17 22:03:36 -0400

    Done.

---
+++ src/components/MovieShow.js
@@ -0,0 +1,24 @@
+import React from 'react';
+ 
+// const MovieShow = props => {
+ 
+//   return (
+//     <div>
+//       <h3>Movies Show Component!</h3>
+//     </div>
+//   );
+// }
+
+
+const MovieShow = ({match, movies}) => {
+  return (
+    <div>
+
+      <h3>{ movies[match.params.movieId].title }</h3>
+    </div>
+  );
+}
+ 
+
+ 
+export default MovieShow;
